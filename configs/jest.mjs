b5ea46ee99f9c24b64b5017aafd9be547beea516
rklos <radoslaw@klos.dev>
2025-03-13 20:03:49 +0100

    Update Jest and Vitest configurations to include support for .tsx and .jsx test files, enhancing testing capabilities for React components.

--- configs/jest.mjs
+++ configs/jest.mjs
@@ -3,7 +3,7 @@ import jest from 'eslint-plugin-jest';
 
 export default [
   {
-    files: [ '**/*.test.ts', '**/*.spec.ts', '**/*.test.mts', '**/*.spec.mts', '**/*.test.js', '**/*.spec.js', '**/*.test.mjs', '**/*.spec.mjs' ],
+    files: [ '**/*.test.ts', '**/*.spec.ts', '**/*.test.mts', '**/*.spec.mts', '**/*.test.tsx', '**/*.spec.tsx', '**/*.test.js', '**/*.spec.js', '**/*.test.mjs', '**/*.spec.mjs', '**/*.test.jsx', '**/*.spec.jsx' ],
     ...jest.configs['flat/recommended'],
     languageOptions: {
       globals: {
@@ -12,7 +12,7 @@ export default [
     },
   },
   {
-    files: [ '**/*.test.ts', '**/*.spec.ts', '**/*.test.mts', '**/*.spec.mts', '**/*.test.js', '**/*.spec.js', '**/*.test.mjs', '**/*.spec.mjs' ],
+    files: [ '**/*.test.ts', '**/*.spec.ts', '**/*.test.mts', '**/*.spec.mts', '**/*.test.tsx', '**/*.spec.tsx', '**/*.test.js', '**/*.spec.js', '**/*.test.mjs', '**/*.spec.mjs', '**/*.test.jsx', '**/*.spec.jsx' ],
     rules: {
       'consistent-return': 0,
     },
--- configs/vitest.mjs
+++ configs/vitest.mjs
@@ -2,7 +2,7 @@ import vitest from '@vitest/eslint-plugin';
 
 export default [
   {
-    files: [ '**/*.test.ts', '**/*.spec.ts', '**/*.test.mts', '**/*.spec.mts', '**/*.test.js', '**/*.spec.js', '**/*.test.mjs', '**/*.spec.mjs' ],
+    files: [ '**/*.test.ts', '**/*.spec.ts', '**/*.test.mts', '**/*.spec.mts', '**/*.test.tsx', '**/*.spec.tsx', '**/*.test.js', '**/*.spec.js', '**/*.test.mjs', '**/*.spec.mjs', '**/*.test.jsx', '**/*.spec.jsx' ],
     plugins: {
       vitest,
     },
